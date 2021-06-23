3425abb718d950914d78f48e6dadf8f58bfb1e07
김종남 책임연구원 AI플랫폼팀 (bellkjn@lgcns.com <bellkjn@CNS.LGCNS.COM>
2021-06-23 16:10:15 +0900

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git reset --hard
 git pull
 
 gcloud app create --region us-central
-gcloud app deploy
+gcloud app deploy -q
 
 PROJECT=$(gcloud config get-value project)
 echo "Visit https://${PROJECT}.appspot.com"
