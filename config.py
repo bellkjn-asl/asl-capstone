5070a503f779c840fc221ef90da5528f4c246ea0
김종남 책임연구원 AI플랫폼팀 (bellkjn@lgcns.com <bellkjn@CNS.LGCNS.COM>
2021-06-23 16:56:23 +0900

    change creditcard model

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ project = "qwiklabs-gcp-04-0e6d92b4880a"
 
 class creditcard_config():
     model_name = "credit_card"
-    version_name = "baseline"
+    version_name = "category_classification"
     predict_key = 'credit'
 
 
