b2d4651f041c8d8c3246005bb477f4527129c4d1
김종남 책임연구원 AI플랫폼팀 (bellkjn@lgcns.com <bellkjn@CNS.LGCNS.COM>
2021-06-24 19:30:22 +0900

    add model info

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ project = "qwiklabs-gcp-04-0e6d92b4880a"
 
 class creditcard_config():
     model_name = "credit_card_model"
-    version_name = "v3"
+    version_name = "v4"
     predict_key = 'prediction'
 
 
